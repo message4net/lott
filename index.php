0b49f1c0165a8edd579f5b2d13201dc61c718a28
messge4net <message4net@outlook.com>
2018-05-30 18:32:49 +0800

    aaaaaaaaaaaaa

--- index.php
+++ index.php
@@ -1,24 +1,24 @@
 <?php //session_start();
-$tmp_bs_dir=dirname(__DIR__).DIRECTORY_SEPARATOR;
-require $tmp_bs_dir.DIRECTORY_SEPARATOR.'conf'.DIRECTORY_SEPARATOR.'init.cfg.php';
-require BASE_DIR.NAME_CFG.DIRECTORY_SEPARATOR.'self.cfg.php';
+//$tmp_bs_dir=dirname(__DIR__).DIRECTORY_SEPARATOR;
+//require $tmp_bs_dir.DIRECTORY_SEPARATOR.'conf'.DIRECTORY_SEPARATOR.'init.cfg.php';
+//require BASE_DIR.NAME_CFG.DIRECTORY_SEPARATOR.'self.cfg.php';
+//
+//require BASE_DIR.NAME_INC.DIRECTORY_SEPARATOR.'init'.POSTFIX_INC;
+//
+//$init=new Init();
 
-require BASE_DIR.NAME_INC.DIRECTORY_SEPARATOR.'init'.POSTFIX_INC;
-
-$init=new Init();
-
-$init->init_app(BASE_DIR.NAME_TMPLT,)
+//$init->init_app(BASE_DIR.NAME_TMPLT,)
 
 
-unset($tmp_bs_dir);
+//unset($tmp_bs_dir);
 
 if (!isset($_SESSION['flag_login'])){
-    require 'templt/login.html';
+    require 'login.html';
 }else{
     if ($_SESSION['flag_login']==1){
         //require '';
         echo 'aaaaaaaaa';
     }else{
-        require '../templt/login.html';
+        require 'login.html';
     }
 }
--- script/login.js
+++ script/login.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
-	$('#btnLogin,#btnSure').click(function(){
-alert($(this).attr('id'));
+	$('#btnLogin,#btnSure,[name=Submit],#btnOut').click(function(){
+//alert($(this).attr('id'));
 	data='lgnbtnid='+$(this).attr('id');
-alert(data);
+//alert(data);
 		$.ajax({
 			type: "POST",
-			url: 'model/login.php',
+			url: 'mdl/login.mdl.php',
 //			url: 'http://127.0.0.2/model/login.php',
 			data: data,
             error: function(XMLHttpRequest, textStatus, errorThrown) {
@@ -15,16 +15,16 @@ alert(data);
             },
 			success: function(msg){
 				//var data=eval('('+msg+')');
-				alert('msg'+msg);
+				//alert('msg'+msg);
 				switch(msg){
 				case('0'):
-					parent.document.location.href = "templt/agree.html"; 
+					parent.document.location.href = "agree.html"; 
 					break;
 				case('1'):
-					parent.document.location.href = "templt/main.html"; 
+					parent.document.location.href = "main.html"; 
 					break;
 				default:
-					parent.document.location.href = "templt/login.html"; 
+					parent.document.location.href = "login.html"; 
 					break;
 				}
 				//if(msg=='zzz'){
